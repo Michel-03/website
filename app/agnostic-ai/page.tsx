c7f15011f0da51739b0a64d427bd793dfdb55e25
KARANAM KARTHIK <karthik.karanam@clr.com>
2026-01-28 21:57:32 +0530

    chnaged the agnostic file0

--- app/agnostic-ai/page.tsx
+++ app/agnostic-ai/page.tsx
@@ -1,15 +1,21 @@
+import { Suspense } from "react";
+
 import HeroSection from "../../OurServices_components/AgnosticAI/Herosection";
 import AgnosticAICom from "../../OurServices_components/AgnosticAI/AgnosticAi";
 import Benifits from "../../OurServices_components/AgnosticAI/Benifits";
 import ServiceDetails from "../../OurServices_components/AgnosticAI/CustomerAnalytics";
 
-export default function AgnosticAI(){
-    return(
-        <>
-        <HeroSection/>
-        <AgnosticAICom/>
-        <Benifits/>
-        <ServiceDetails/>
-        </>
-    )
+export default function AgnosticAI() {
+  return (
+    <>
+      <HeroSection />
+      <AgnosticAICom />
+      <Benifits />
+
+      {/* ✅ ONLY THIS PART NEEDS SUSPENSE */}
+      <Suspense fallback={<div className="py-20 text-center">Loading...</div>}>
+        <ServiceDetails />
+      </Suspense>
+    </>
+  );
 }
