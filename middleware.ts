9d6e2de5304a00f26bdb49f9cdff3b7381a38329
KARANAM KARTHIK <karthik.karanam@clr.com>
2026-01-29 14:19:32 +0530

    updated code for linked in

--- middleware.ts
+++ middleware.ts
@@ -6,12 +6,18 @@ export function middleware(req: NextRequest) {
     console.log("MIDDLEWARE RUNNING → Path:", req.nextUrl.pathname);
   const pathname = req.nextUrl.pathname;
 
+   const userAgent = req.headers.get("user-agent") || "";
   // Allow login page and login API
   if (
     pathname.startsWith("/admin/login") ||
     pathname.startsWith("/api/admin/login")
   ) {
     console.log("RETURNED THE DATA")
+    return NextResponse.next();
+  }
+
+    // ✅ Allow LinkedIn crawler
+  if (userAgent.includes("LinkedInBot")) {
     return NextResponse.next();
   }
 
